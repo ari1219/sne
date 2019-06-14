72e5412dc1798d7ba2fcae58f039cb93bed0dfe5
AriharaHiroki <arihara.hiroki.lab@gmail.com>
2019-06-14 18:07:08 +0900

    first commit

--- src/model.py
+++ src/model.py
@@ -18,21 +18,23 @@ class Model:
         self.build_output_embedding()
 
     def build_valiable(self):
-        bound = 6 / math.sqrt(self.embedding_dim)
+        _bound = 6 / math.sqrt(self.embedding_dim)
         self.s = tf.placeholder(tf.int32, shape=[None])
         self.t = tf.placeholder(tf.int32, shape=[None])
         self.r = tf.placeholder(tf.int32, shape=[None])
-        self.emb_s = tf.Variable(tf.random_uniform([num_of_nodes, d], minval=-bound, maxval=bound), dtype=tf.float32)
-        self.emb_t = tf.Variable(tf.random_uniform([num_of_nodes, d], minval=-bound, maxval=bound), dtype=tf.float32)
-        self.emb_r = tf.Variable(tf.ranodm_uniform([2, d], minval=-bound, maxval=bound), dtype=tf.float32)
-        self.W_sr = tf.Variable(tf.random_uniform([d, d], minval=-bound, maxval=bound), dtype=tf.flaot32)
-        self.W_st = tf.Variable(tf.ranodm_uniform([d, d], minval=bound, maxval=bound), dtype=tf.float32)
+        self.emb_s = tf.Variable(tf.random_uniform([num_of_nodes, d], minval=-_bound, maxval=_bound), dtype=tf.float32)
+        self.emb_t = tf.Variable(tf.random_uniform([num_of_nodes, d], minval=-_bound, maxval=_bound), dtype=tf.float32)
+        self.emb_r = tf.Variable(tf.ranodm_uniform([2, d], minval=-_bound, maxval=_bound), dtype=tf.float32)
+        self.W_sr = tf.Variable(tf.random_uniform([d, d], minval=-_bound, maxval=_bound), dtype=tf.flaot32)
+        self.W_st = tf.Variable(tf.ranodm_uniform([d, d], minval=_bound, maxval=_bound), dtype=tf.float32)
 
-    def build_graph(self):
-        self.loss_rel = self.build_loss_rel()
+    def build_model(self):
         self.loss_skip = self.build_loss_skip()
+        self.loss_rel = self.build_loss_rel()
         self.loss_reg = self.build_loss_reg()
         self.total_loss = self.loss_skip + self.alpha * self.loss_rel + self.lam * self.loss_reg
-        self.train = self.build_train()
 
     def build_loss_reg(self):
+        self.s_vec = tf.nn.embedding_lookup(self.s_emb, self.s)
+        self.r_vec = tf.nn.embedding_lookup(self.r_emb, self.r)
+        self.f =
